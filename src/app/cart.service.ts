f36ed920574d37084f57be08b7a327d0f7a0416c
shreya1864 <1864.sc@gmail.com>
2021-02-05 12:11:02 +0530

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,30 +1,41 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http"
+import { Inject, Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LOCAL_STORAGE, StorageService } from "ngx-webstorage-service";
 
 @Injectable()
-export class CartService {
+export class CartService implements OnInit {
+
   items = [];
   constructor(
+    @Inject(LOCAL_STORAGE) private storage: StorageService,
     private http: HttpClient
-  ) {}
+  ) { }
 
-  //Adding data to cart
-  addToCart(product) {
+  ngOnInit(){
+    this.items = this.storage.get('myCart') ? this.storage.get('myCart') : [];
+  }
+
+//Adding data to cart
+  addToCart(product){
     this.items.push(product);
+    this.storage.set("myCart", this.items);
   }
 
-  //getting data from the cart
-  getItems() {
+//getting data from the cart
+  getItems(){
+    this.storage.get("myCart");
     return this.items;
   }
 
-  //clear the cart data
-  clearCart() {
+//clear the cart data
+  clearCart(){
     this.items = [];
+    this.storage.remove("myCart");
     return this.items;
   }
 
-  getShippingPrices() {
-    return this.http.get('./assets/shipping.json')
+  getShippingPrices(){
+    return this.http.get('./assets/shipping.json');
   }
+
 }
