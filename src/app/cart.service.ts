8b36dd173c63feae1dd5441e9fd4f51782d98bf3
shreya1864 <1864.sc@gmail.com>
2021-02-05 13:45:27 +0530

    3 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,41 +1,33 @@
 import { Inject, Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { LOCAL_STORAGE, StorageService } from "ngx-webstorage-service";
-
 @Injectable()
 export class CartService implements OnInit {
-
-  items = [];
-  constructor(
-    @Inject(LOCAL_STORAGE) private storage: StorageService,
-    private http: HttpClient
-  ) { }
-
-  ngOnInit(){
-    this.items = this.storage.get('myCart') ? this.storage.get('myCart') : [];
-  }
-
+items = [];
+constructor(
+@Inject(LOCAL_STORAGE) private storage: StorageService,
+private http: HttpClient
+) { }
+ngOnInit(){
+this.items = this.storage.get('myCart') ? this.storage.get('myCart') : [];
+}
 //Adding data to cart
-  addToCart(product){
-    this.items.push(product);
-    this.storage.set("myCart", this.items);
-  }
-
+addToCart(product){
+this.items.push(product);
+this.storage.set("myCart", this.items);
+}
 //getting data from the cart
-  getItems(){
-    this.storage.get("myCart");
-    return this.items;
-  }
-
+getItems(){
+this.storage.get("myCart");
+return this.items;
+}
 //clear the cart data
-  clearCart(){
-    this.items = [];
-    this.storage.remove("myCart");
-    return this.items;
-  }
-
-  getShippingPrices(){
-    return this.http.get('./assets/shipping.json');
-  }
-
+clearCart(){
+this.items = [];
+this.storage.remove("myCart");
+return this.items;
+}
+getShippingPrices(){
+return this.http.get('./assets/shipping.json');
+}
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,47 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { LOCAL_STORAGE, StorageService } from "ngx-webstorage-service";
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+selector: 'app-cart',
+templateUrl: './cart.component.html',
+styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+// items = this.cartService.getItems();
+items = [];
+checkoutForm = this.formBuilder.group({
+name: ['', Validators.required],
+address: ['', Validators.required]
+})
 
-  items = this.cartService.getItems();
-  checkoutForm = this.formBuilder.group({
-    name: ['',Validators.required],
-    address: ['',Validators.required]
-  })
-  
-  constructor(
-    private cartService: CartService,
-    private formBuilder: FormBuilder,
-    private router: Router
-  ) { }
-
-  ngOnInit() {
-  }
-
-  routeTo400(){
-    this.router.navigateByUrl('page-not-found');
-  }
-
-  routeTo500(){
-    this.router.navigateByUrl('internal-server-error');
-  }
-
-  routeToGenericError(){
-    this.router.navigateByUrl('error');
-  }
-
-  onSubmit(): void{
-    //Clear the Cart
-    this.items = this.cartService.clearCart();
-    console.warn('Your oder has been submitted', this.checkoutForm.value);
-    this.checkoutForm.reset();
-  }
-
+constructor(
+private cartService: CartService,
+private formBuilder: FormBuilder,
+private router: Router,
+@Inject(LOCAL_STORAGE) private storage: StorageService
+) { }
+ngOnInit() {
+this.items = this.storage.get("myCart");
+}
+routeTo400(){
+this.router.navigateByUrl('page-not-found');
+}
+routeTo500(){
+this.router.navigateByUrl('internal-server-error');
+}
+routeToGenericError(){
+this.router.navigateByUrl('error');
+}
+onSubmit(): void{
+//Clear the Cart
+this.items = this.cartService.clearCart();
+console.warn('Your oder has been submitted', this.checkoutForm.value);
+this.checkoutForm.reset();
+}
 }
